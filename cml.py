05f88f655ccc071e10f657136dca600c4ba07f4e
lidingke <lidingke@hust.edu.cn>
2016-05-02 22:35:10 +0800

    add cmd command list2

--- cml.py
+++ cml.py
@@ -1,9 +1,11 @@
-from mulpro import God
-from pandaspider import PandaSpider
+# from mulpro import God
+# from pandaspider import PandaSpider
 from pandaTV import PandaTV
 
-from douyuspider import DouyuSpider
+# from douyuspider import DouyuSpider
 from douyuTV import DouyuTV
+
+from singler import DouyuGuard , PandaGuard, God
 
 import sys
 
@@ -12,9 +14,9 @@ if __name__ == '__main__':
     cmd = sys.argv[1] if len(sys.argv)>1 else 'None'
     rid = sys.argv[1] if len(sys.argv)>1 else None
     if cmd == 'douyuGuard':
-        God(DouyuSpider()).run()
+        God(DouyuGuard()).run()
     elif cmd =='pandaGuard':
-        God(PandaSpider()).run()
+        God(PandaGuard()).run()
     elif cmd == 'douyuDanmu':
         roomid= rid if len(rid)>1 else '48699'
         DouyuTV(roomid,show=True).start()
@@ -22,8 +24,8 @@ if __name__ == '__main__':
         roomid= rid if len(rid)>1 else '66666'
         PandaTV(roomid,show=True).start()
     elif cmd == 'douyuNoGuard':
-        DouyuSpider.run()
+        DouyuGuard.run()
     elif cmd == 'pandaNoGuard':
-        PandaSpider.run()
+        PandaGuard.run()
     else:
         print('Command is not found')
--- script.py
+++ script.py
@@ -5,8 +5,3 @@ God(PandaGuard()).run()
 if __name__ == '__main__':
     God(PandaGuard()).run()
 
-
-# from mulpro import God
-# from singler import DouyuGuard
-
-# God(DouyuGuard()).run()
